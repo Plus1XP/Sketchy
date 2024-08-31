6d763e2c437f5a83fb71d76d2800729c14019e49
Plus1XP <evldev@protonmail.ch>
2024-08-31 17:20:35 +0100

    Refactor: SettingsView

--- Sketchy/View/SettingsView.swift
+++ Sketchy/View/SettingsView.swift
@@ -25,121 +25,126 @@ struct SettingsView: View {
     }()
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Settings")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.leading, 20)
-            Form {
-                Section(header: Text("\(Image(systemName: "message")) FeedBack")) {
-                    Group {
-                        HStack {
-                            Link(destination: URL(string: self.mailURL)!) {
-                                HStack {
-                                    Image(systemName: "envelope")
-                                        .foregroundStyle(.blue)
-                                    Text("Get in Touch")
-                                        .foregroundColor(.primary)
-                                }
-                            }
-                        }
-                        HStack {
-                            Link(destination: URL(string: self.supportURL)!) {
-                                HStack {
-                                    Image(systemName: "safari")
-                                        .foregroundStyle(.red, .blue)
-                                        .font(.title2)
-                                    Text("Discover More")
-                                        .foregroundColor(.primary)
-                                }
-                            }
-                        }
-                        HStack {
-                            ShareLink(
-                                item: URL(string: self.appURL + self.appID)!,
-                                preview: SharePreview( self.appName,
-                                                       image: Image(uiImage: UIImage(named: "AppIcon60x60") ?? UIImage())
-                                                     )
-                            ) {
-                                HStack {
-                                    Image(systemName: "square.and.arrow.up")
-                                        .foregroundStyle(.blue)
-                                        .font(.title2)
-                                    Text("Share with Friends")
-                                        .foregroundColor(.primary)
-                                }
-                            }
-                        }
-                        HStack {
-                            Link(destination: URL(string: self.appURL + self.appID + self.reviewForwarder)!) {
-                                HStack {
-                                    Image(systemName: "star.fill")
-                                        .foregroundStyle(.yellow)
-                                        .font(.title3)
-                                    Text("Rate and Review")
-                                        .foregroundColor(.primary)
-                                }
-                            }
-                        }
-                    }
-                }
-                Section(header: Text("\(Image(systemName: "info.circle")) About")) {
-                    Group {
-                        HStack {
-                            Image(systemName: "exclamationmark.triangle.fill")
-                                .foregroundStyle(.black, .yellow)
-                            Text("Version \(self.versionString)")
-                        }
-                        HStack {
-                            Link(destination: URL(string: self.githubURL)!) {
-                                HStack {
-                                    Image(systemName: "paintbrush.fill")
-                                        .foregroundStyle(.green)
-                                    Text("Designed by Plus1XP")
-                                        .foregroundColor(.primary)
-                                }
-                            }
-                        }
-                        HStack {
-                            Link(destination: URL(string: self.githubURL)!) {
-                                HStack {
-                                    Image(systemName: "hammer.fill")
-                                        .foregroundStyle(.gray)
-                                    Text("Developed by Plus1XP")
-                                        .foregroundColor(.primary)
-                                }
-                            }
-                        }
-                        HStack {
-                            Image(systemName: "c.circle")
-                                .foregroundStyle(.primary)
-                            Text("Copyright 2023 Plus1XP")
-                        }
-                    }
-                }
-                if checkTodayIsSpecialDay(day: 16, month: 4) {
+        Form {
+            HStack {
+                Image(systemName: "questionmark.circle")
+                    .foregroundStyle(.blue)
+                    .font(.title)
+                Text("Help")
+                    .foregroundColor(.primary)
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            // Removes white form section backgroung
+            .listRowBackground(Color.clear)
+            
+            Section(header: Text("\(Image(systemName: "message")) FeedBack")) {
+                Group {
                     HStack {
-                        Spacer()
-                        Button("🎁", action: {
-                            self.showConfetti = true
-                        })
-                        .buttonStyle(GrowingIconButtonStyle())
-                        Spacer()
+                        Link(destination: URL(string: self.mailURL)!) {
+                            HStack {
+                                Image(systemName: "envelope")
+                                    .foregroundStyle(.blue)
+                                Text("Get in Touch")
+                                    .foregroundColor(.primary)
+                            }
+                        }
                     }
-                    // Needed to fix bug of section acting as a big button.
-                    .buttonStyle(BorderlessButtonStyle())
-                    // Removes white form section backgroung
-                    .listRowBackground(Color.clear)
-                    
+                    HStack {
+                        Link(destination: URL(string: self.supportURL)!) {
+                            HStack {
+                                Image(systemName: "safari")
+                                    .foregroundStyle(.red, .blue)
+                                    .font(.title2)
+                                Text("Discover More")
+                                    .foregroundColor(.primary)
+                            }
+                        }
+                    }
+                    HStack {
+                        ShareLink(
+                            item: URL(string: self.appURL + self.appID)!,
+                            preview: SharePreview( self.appName,
+                                                   image: Image(uiImage: UIImage(named: "AppIcon60x60") ?? UIImage())
+                                                 )
+                        ) {
+                            HStack {
+                                Image(systemName: "square.and.arrow.up")
+                                    .foregroundStyle(.blue)
+                                    .font(.title2)
+                                Text("Share with Friends")
+                                    .foregroundColor(.primary)
+                            }
+                        }
+                    }
+                    HStack {
+                        Link(destination: URL(string: self.appURL + self.appID + self.reviewForwarder)!) {
+                            HStack {
+                                Image(systemName: "star.fill")
+                                    .foregroundStyle(.yellow)
+                                    .font(.title3)
+                                Text("Rate and Review")
+                                    .foregroundColor(.primary)
+                            }
+                        }
+                    }
                 }
             }
+            Section(header: Text("\(Image(systemName: "info.circle")) About")) {
+                Group {
+                    HStack {
+                        Image(systemName: "exclamationmark.triangle.fill")
+                            .foregroundStyle(.black, .yellow)
+                        Text("Version \(self.versionString)")
+                    }
+                    HStack {
+                        Link(destination: URL(string: self.githubURL)!) {
+                            HStack {
+                                Image(systemName: "paintbrush.fill")
+                                    .foregroundStyle(.green)
+                                Text("Designed by Plus1XP")
+                                    .foregroundColor(.primary)
+                            }
+                        }
+                    }
+                    HStack {
+                        Link(destination: URL(string: self.githubURL)!) {
+                            HStack {
+                                Image(systemName: "hammer.fill")
+                                    .foregroundStyle(.gray)
+                                Text("Developed by Plus1XP")
+                                    .foregroundColor(.primary)
+                            }
+                        }
+                    }
+                    HStack {
+                        Image(systemName: "c.circle")
+                            .foregroundStyle(.primary)
+                        Text("Copyright 2023 Plus1XP")
+                    }
+                }
+            }
+            if checkTodayIsSpecialDay(day: 16, month: 4) {
+                HStack {
+                    Spacer()
+                    Button("🎁", action: {
+                        self.showConfetti = true
+                    })
+                    .buttonStyle(GrowingIconButtonStyle())
+                    Spacer()
+                }
+                // Needed to fix bug of section acting as a big button.
+                .buttonStyle(BorderlessButtonStyle())
+                // Removes white form section backgroung
+                .listRowBackground(Color.clear)
+                
+            }
         }
-        .padding(.top)
         .presentationDragIndicator(.visible)
-        .background(self.colorScheme == .light ? Color(UIColor.secondarySystemBackground) : Color(UIColor.systemBackground))
         .displayConfetti(isActive: $showConfetti)
     }
 }
+
 #Preview {
     SettingsView()
 }
