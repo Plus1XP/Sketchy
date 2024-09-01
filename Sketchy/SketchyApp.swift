490ad0cf1d1d5c210897e7cbfddb727a0a3e4bf5
Plus1XP <evldev@protonmail.ch>
2024-09-01 15:11:08 +0100

    For apperance fix

--- Sketchy/SketchyApp.swift
+++ Sketchy/SketchyApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct SketchyApp: App {
+    @AppStorage("appearance") var appearance: AppearanceType = .automatic
+
     var body: some Scene {
         DocumentGroup(newDocument: Drawing.init) { file in
             ContentView()
+                .preferredColorScheme(appearance.colorScheme)
+                .environment(\.colorScheme, appearance.colorScheme ?? .light)
+                .onChange(of: appearance, {
+                    debugPrint("Apperance changed")
+                    let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                    if let firstWindow = scene?.windows.first {
+                        firstWindow.overrideUserInterfaceStyle = appearance.userInerfaceStyle ?? .unspecified
+                    }
+                })
+                .onAppear {
+                    let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                    if let firstWindow = scene?.windows.first {
+                        firstWindow.overrideUserInterfaceStyle = appearance.userInerfaceStyle ?? .unspecified
+                    }
+                }
         }
     }
 }
