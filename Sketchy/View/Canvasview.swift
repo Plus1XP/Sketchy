117829e669a6976ab23de9708806fd2f9d90594c
Plus1XP <evldev@protonmail.ch>
2024-09-12 14:53:15 +0100

    Refactor: Split CanvasView with extension class

--- Sketchy/View/Canvasview.swift
+++ Sketchy/View/Canvasview.swift
@@ -19,7 +19,7 @@ struct CanvasView: View {
     @State private var canShowSettingsView: Bool = false
     @State private var canShowDeleteAlert: Bool = false
     @State private var canExpandUndoBar: Bool = false
-
+    
     var body: some View {
         Canvas { context, size in
             context.fill(Path(CGRect(origin: .zero, size: size)), with: .color(self.drawing.backgroundColor))
@@ -143,133 +143,6 @@ struct CanvasView: View {
             feedbackGenerator?.notificationOccurred(.success)
             self.canShowSettingsView.toggle()
         }
-    }
-    
-    private func setSafeAreas(canIgnoreSafeArea: Bool) -> Edge.Set {        
-        if self.drawing.overideFullSizeCanvas(userPrefs: canIgnoreSafeArea) {
-            Edge.Set.all
-        } else {
-            []
-        }
-    }
-    
-    private func setDeviceOrientation(orientation: OrientationType) {
-        DispatchQueue.main.async {
-            switch orientation {
-            case .automatic:
-                AppDelegate.orientationLock = UIInterfaceOrientationMask.all
-            case .portrait:
-                AppDelegate.orientationLock = UIInterfaceOrientationMask.portrait
-            case .landscape:
-                AppDelegate.orientationLock = UIInterfaceOrientationMask.landscape
-            }
-            UIViewController.attemptRotationToDeviceOrientation()
-        }
-    }
-    
-    private func resetDeviceOrientation() {
-        DispatchQueue.main.async {
-            AppDelegate.orientationLock = UIInterfaceOrientationMask.allButUpsideDown
-            UIViewController.attemptRotationToDeviceOrientation()
-        }
-    }
-    
-    private func drawStroke(_ stroke: Stroke, in context: GraphicsContext) {
-        switch stroke.tool {
-        case .brush:
-            self.drawBrush(stroke, in: context)
-        case .circle:
-            self.drawCircle(stroke, in: context)
-        case .diamond:
-            self.drawPolygon(stroke, in: context)
-        case .eraser:
-            self.drawEraser(stroke, in: context)
-        case .hexagon:
-            self.drawPolygon(stroke, in: context)
-        case .line:
-            self.drawLine(stroke, in: context)
-        case .octagon:
-            self.drawPolygon(stroke, in: context)
-        case .pentagon:
-            self.drawPolygon(stroke, in: context)
-        case .star:
-            self.drawPolygon(stroke, in: context)
-        case .square:
-            self.drawPolygon(stroke, in: context)
-        case .triangle:
-            self.drawPolygon(stroke, in: context)
-        }
-    }
-    
-    // Method to draw brush strokes
-    private func drawBrush(_ stroke: Stroke, in context: GraphicsContext) {
-        let path = Path(curving: stroke.points)
-        
-        var contextCopy = context
-        if stroke.blur > 0 {
-            contextCopy.addFilter(.blur(radius: stroke.blur))
-        }
-        contextCopy.stroke(path, with: .color(stroke.color), style: StrokeStyle(lineWidth: stroke.width, lineCap: .round, lineJoin: .round, dash: [1, stroke.spacing * stroke.width])
-        )
-    }
-    
-    // Method to draw eraser strokes
-    private func drawEraser(_ stroke: Stroke, in context: GraphicsContext) {
-        let path = Path(curving: stroke.points)
-        
-        var contextCopy = context
-        if stroke.blur > 0 {
-            contextCopy.addFilter(.blur(radius: stroke.blur))
-        }
-        contextCopy.stroke(path, with: .color(self.drawing.backgroundColor), style: StrokeStyle(lineWidth: stroke.width, lineCap: .round, lineJoin: .round, dash: [1, stroke.spacing * stroke.width])
-        )
-    }
-    
-    // Method to draw circles
-    private func drawCircle(_ stroke: Stroke, in context: GraphicsContext) {
-        guard let center = stroke.points.first, let radiusPoint = stroke.points.last else { return }
-        let radius = hypot(radiusPoint.x, radiusPoint.y)
-        let circlePath = Path(ellipseIn: CGRect(x: center.x - radius, y: center.y - radius, width: radius * 2, height: radius * 2))
-        
-        var contextCopy = context
-        if stroke.blur > 0 {
-            contextCopy.addFilter(.blur(radius: stroke.blur))
-        }
-        if stroke.fill {
-            context.fill(circlePath, with: .color(stroke.fillColor))
-        }
-        contextCopy.stroke(circlePath, with: .color(stroke.color), style: StrokeStyle(lineWidth: stroke.width, lineCap: .round, lineJoin: .round, dash: [1, stroke.spacing * stroke.width])
-        )
-    }
-    
-    // Method to draw lines
-    private func drawLine(_ stroke: Stroke, in context: GraphicsContext) {
-        var path = Path()
-        guard let startPoint = stroke.points.first, let endPoint = stroke.points.last else { return }
-        path.move(to: startPoint)
-        path.addLine(to: endPoint)
-        
-        var contextCopy = context
-        if stroke.blur > 0 {
-            contextCopy.addFilter(.blur(radius: stroke.blur))
-        }
-        contextCopy.stroke(path, with: .color(stroke.color), style: StrokeStyle(lineWidth: stroke.width, lineCap: .round, lineJoin: .round, dash: [1, stroke.spacing * stroke.width])
-        )
-    }
-    
-    private func drawPolygon(_ stroke: Stroke, in context: GraphicsContext) {
-        var path = Path()
-        path.addLines(stroke.points)
-        
-        var contextCopy = context
-        if stroke.blur > 0 {
-            contextCopy.addFilter(.blur(radius: stroke.blur))
-        }
-        if stroke.fill {
-            contextCopy.fill(path, with: .color(stroke.fillColor))
-        }
-        contextCopy.stroke(path, with: .color(stroke.color), style: StrokeStyle(lineWidth: stroke.width, lineCap: .round, lineJoin: .round, dash: [1, stroke.spacing * stroke.width])
-        )
     }
 }
 
